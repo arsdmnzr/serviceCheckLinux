2dea15b74ca6153db0e071b4dc684d393e7d4334
arsdmnzr <arshadmanzoor@rocketmail.com>
2025-05-16 16:01:33 +0530

    initial commit

---
+++ service_status.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+service_check () {
+service="$1"
+if ! systemctl is-active --quiet $service; then
+echo "$service is down, starting again"
+systemctl start $service
+fi
+}
+ 
+service_check ssh
